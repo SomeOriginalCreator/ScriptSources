ccdfd586347ded5cb8fe1e52b711e89952095ebd
SomeOriginalCreator <102445176+SomeOriginalCreator@users.noreply.github.com>
2026-01-31 16:45:58 -0800

    Create RushSpawn.lua

---
+++ RushSpawn.lua
@@ -0,0 +1,57 @@
+---====== Load spawner ======---
+
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+
+---====== Create entity ======---
+
+local entity = spawner.Create({
+	Entity = {
+		Name = "Black Rush",
+		Asset = "https://github.com/SomeOriginalCreator/ScriptSources/raw/refs/heads/main/Ruh.rbxm",
+		HeightOffset = 0
+	},
+	Lights = {
+		Flicker = {
+			Enabled = true,
+			Duration = 1
+		},
+		Shatter = true,
+		Repair = false
+	},
+	Earthquake = {
+		Enabled = true
+	},
+	CameraShake = {
+		Enabled = true,
+		Range = 100,
+		Values = {1.5, 20, 0.1, 1} -- Magnitude, Roughness, FadeIn, FadeOut
+	},
+	Movement = {
+		Speed = 100,
+		Delay = 2,
+		Reversed = false
+	},
+	Rebounding = {
+		Enabled = true,
+		Type = "Ambush", -- "Blitz"
+		Min = 10,
+		Max = 20,
+		Delay = 2
+	},
+	Damage = {
+		Enabled = true,
+		Range = 40,
+		Amount = 125
+	},
+	Crucifixion = {
+		Enabled = true,
+		Range = 40,
+		Resist = false,
+		Break = true
+	},
+	Death = {
+		Type = "Guiding", -- "Curious"
+		Hints = {"You're such a fucking nigger.", "How tf did u to ts nigga vrotato.", "This is why you belong on the farms.", "Such a stupid nigger."},
+		Cause = "Nigger Killer"
+	}
+})
