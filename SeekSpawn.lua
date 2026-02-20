8942141b47493683dcdabfc1478259969f19cfa7
SomeOriginalCreator <102445176+SomeOriginalCreator@users.noreply.github.com>
2026-02-20 12:46:13 -0800

    Update SeekSpawn.lua

--- SeekSpawn.lua
+++ SeekSpawn.lua
@@ -1,59 +1,22 @@
---Initiate
---Seek Specific
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
-local curRoom = workspace.RoomNum.Value
-local spawnRoom = curRoom - 5
-local nodeRoom = spawnRoom + 1
-local entityNodes = workspace.entityNodes
-local targetNode = entityNodes[tostring(nodeRoom)]["1"]
-local rs = game:GetService("RunService")
-local folder = nodeRoom
-local isHiding = false
-local rebounding = false
-local swtOver = false
 
---Entity settings
-
-local url = "https://github.com/SomeOriginalCreator/ScriptSources/raw/refs/heads/main/SeekYellow%20.rbxm" --Url of entity model
-local spawnWaitTime = 1 --Wait time for entity to move after spawning
-local canKill = true
-local rebound = false --Entity will rebound if true (like ambush)
-local rebounds = 1 --Amount of rebounds the entity will do
-local reboundWaitTime = 1 --Amount of time the entity will wait before rebounding
-local audioCue = "0" --put in an audio id to play when the entity spawns 0 means no sound will play
---local flickerLights = false --Will flicker lights when entity spawns
---local flickerDuration = 1 --Duration of light flicker
-local entitySpeed = 1.5 --Speed of entity
-
---Do the cues
-
-audioCue = "rbxassetid://" .. audioCue
-
-local sound = Instance.new("Sound")
-sound.Parent = workspace
-local soundPart = Instance.new("Part")
-soundPart.Anchored = true
-soundPart.CanCollide = false
-soundPart.Transparency = 1
-soundPart.Position = game.Players.LocalPlayer.Character.Head.CFrame.Position
-sound.SoundId = audioCue
-sound.Looped = false
-wait(0.1)
-sound:Play()
-
---Spawn entity
-
-local entity = LoadCustomInstance(url)
-entity.Figure.CFrame = CFrame.new(workspace.CurrentRooms[tostring(spawnRoom)].Door.Door.CFrame.Position)
-entity.Parent = workspace
-local cPart = entity.Figure
-entity.Figure.Footsteps:Play()
-entity.Figure.FootstepsFar:Play()
-entity.SeekMusic.TimePosition = 0
-
---Start seek animations
-
-task.spawn(function() -- New thread for seek animation
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/SomeOriginalCreator/ScriptSources/refs/heads/main/Spawner.lua"))()
+spawner.changeSettings(
+	"https://github.com/SomeOriginalCreator/ScriptSources/raw/refs/heads/main/SeekYellow%20.rbxm", --EntityModelUrl
+	1, --Spawn wait time
+	false, --Can kill
+	false,--Will rebound
+	2, --Amount of rebounds
+	1, --Rebound wait time
+	"0", --AudioCue soundid
+	1,-- EntitySpeed
+	false, --FlickerLights
+	1, --FlickerDuration
+function()
+	local entity = spawner.entityModel()
+	entity.Figure.Footsteps:Play()
+	entity.Figure.FootstepsFar:Play()
+	entity.SeekMusic.TimePosition = 0
+	task.spawn(function() -- New thread for seek animation
 	if getgenv()["Animator"] == nil then
 		print("Running animator api")
     	loadstring(game:HttpGet("https://raw.githubusercontent.com/xhayper/Animator/main/Source/Main.lua"))()
@@ -64,188 +27,11 @@ task.spawn(function() -- New thread for seek animation
 	runTrack.Looped = true
 	runTrack:Play()
 end)
-
---Start entity
-
-wait(spawnWaitTime)
-swtOver = true
---Functions
-function addY(vect, add)
-	local newvec = Vector3.new(vect.x, vect.y + add, vect.z)
-	return newvec
-end
-function getDistance(v1, v2)
-	return math.abs((v1 - v2).Magnitude)
-end
---Controller
---while wait(0.001) and swtOver do
-rs.Heartbeat:Connect(function()
-    if swtOver then
-	curRoom = workspace.RoomNum.Value
-	wnodeRoom = curRoom - 4
-    if not rebounding then
-    isHiding = false
-    --print(getDistance(entity.RushNew.CFrame.Position, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position))
-    --[[for i, v in workspace:GetDescendants() do
-        if v.Name == "HiddenPlayer" and v:IsA("ObjectValue") then
-            if v.Value == game.Players.LocalPlayer.Character then
-                isHiding = true
-            end
-        end
-    end]]
-    if canKill and workspace.isHiding.Value == false and getDistance(entity.Figure.CFrame.Position, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position) <= 25 then
-        --game.Players.LocalPlayer.Character.Humanoid.Health = 0
-        print("in range, raycasting")
-        local rayOrigin = cPart.Position
-        local rayDest = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-	    local rayDir = (rayDest - rayOrigin).Unit * 25
-	    local rayParams = RaycastParams.new()
-	    local rayResults = workspace:Raycast(rayOrigin, rayDir, rayParams)
-        --print(rayResults)
-        if rayResults ~= nil and rayResults.Instance.Parent == game.Players.LocalPlayer.Character then
-            print("killing")
-            game.Players.LocalPlayer.Character.Humanoid.Health = 0
-            wait(1.2)
-			firesignal(game.ReplicatedStorage.RemotesFolder.DeathHint.OnClientEvent, {"You died to seek vro", "ts one only slightly harder", "are we srs vro 🥀", "I hope to not see u again twin🙏"}, "Blue")
-        end
-	    --print(rayResults)
-    end
-    currentRoom = workspace.RoomNum.Value
-    spawnRoom = currentRoom - 5
-    nodeRoom = spawnRoom + 1
-    cPart.CFrame = CFrame.lookAt(cPart.CFrame.Position, addY(targetNode.CFrame.Position, 4))
-	cPart.CFrame = cPart.CFrame + cPart.CFrame.LookVector * entitySpeed
-    local oldTN = targetNode
-    if getDistance(cPart.CFrame.Position, addY(targetNode.CFrame.Position, 4)) <= entitySpeed then
-        if targetNode.Name == "500" then
-            swtOver = false
-            entity:Destroy()
-            targetNode:Destroy()
-            --error("This isn't an actual error, this is just to stop the script execution.")
-            canKill = false
-            return
-        end
-		for i, v in targetNode.Parent:GetChildren() do
-		    if tonumber(v.Name) == tonumber(targetNode.Name) + 1 then
-		    	targetNode = v
-		    end
-		end
-		if targetNode == oldTN and workspace.entityNodes:FindFirstChild(tostring(folder + 1)) then
-            if folder == curRoom then
-                if rebound and rebounds >= 1 then
-                    wait(reboundWaitTime)
-                    rebounding = true
-                    rebounds -= 1
-                    for i, v in targetNode.Parent:GetChildren() do
-		                if tonumber(v.Name) == tonumber(targetNode.Name) - 1 then
-		    	            targetNode = v
-		                end
-		            end
-                    if targetNode == oldTN and workspace.entityNodes:FindFirstChild(tostring(folder - 1)) then
-                        if folder == curRoom then
-                            folder -= 1
-                            local hn = -5
-                            for i, v in workspace.entityNodes:FindFirstChild(tostring(folder)):GetChildren() do
-                                if tonumber(v.Name) > hn then
-                                    hn = tonumber(v.Name)
-                                end
-                            end 
-                            targetNode = workspace.entityNodes[tostring(folder)][tostring(hn)]
-                        end
-                    end
-                else
-                    sound:Destroy()
-                    local ntn = Instance.new("Part")
-                    ntn.Anchored = true
-                    ntn.Parent = workspace
-                    ntn.Name = "500"
-                    ntn.CFrame = CFrame.new(addY(targetNode.CFrame.Position, -300))
-                    targetNode = ntn
-                end
-            else
-                folder += 1
-			    targetNode = workspace.entityNodes[tostring(folder)]["1"]
-            end
-		elseif targetNode == oldTN then
-            sound:Destroy()
-			local ntn = Instance.new("Part")
-            ntn.Anchored = true
-            ntn.Parent = workspace
-            ntn.Name = "50"
-            ntn.CFrame = CFrame.new(addY(targetNode.CFrame.Position, -300))
-            targetNode = ntn
-		end
-	end
-    
---end)
-    else
---rs.Heartbeat:Connect(function()
-    isHiding = false
-    --print(getDistance(entity.RushNew.CFrame.Position, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position))
-    --[[for i, v in workspace:GetDescendants() do
-        if v.Name == "HiddenPlayer" and v:IsA("ObjectValue") then
-            if v.Value == game.Players.LocalPlayer.Character then
-                isHiding = true
-            end
-        end
-    end]]
-    if canKill and workspace.isHiding.Value == false and getDistance(entity.Figure.CFrame.Position, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position) <= 75 then
-        --game.Players.LocalPlayer.Character.Humanoid.Health = 0
-        print("in range, raycasting")
-        local rayOrigin = cPart.Position
-        local rayDest = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-	    local rayDir = (rayDest - rayOrigin).Unit * 75
-	    local rayParams = RaycastParams.new()
-	    local rayResults = workspace:Raycast(rayOrigin, rayDir, rayParams)
-        --print(rayResults)
-        if rayResults ~= nil and rayResults.Instance.Parent == game.Players.LocalPlayer.Character then
-            print("killing")
-            game.Players.LocalPlayer.Character.Humanoid.Health = 0
-            wait(1.2)
-			firesignal(game.ReplicatedStorage.RemotesFolder.DeathHint.OnClientEvent, {"Skill issue, u gotta be so bad to die to that", "Fuhhin nigga", "You so god damn bad vro"}, "Blue")
-        end
-	    --print(rayResults)
-    end
-    currentRoom = workspace.RoomNum.Value
-    spawnRoom = currentRoom - 5
-    nodeRoom = spawnRoom + 1
-    cPart.CFrame = CFrame.lookAt(cPart.CFrame.Position, addY(targetNode.CFrame.Position, 4))
-	cPart.CFrame = cPart.CFrame + cPart.CFrame.LookVector * entitySpeed
-    local oldTN = targetNode
-    if getDistance(cPart.CFrame.Position, addY(targetNode.CFrame.Position, 4)) <= entitySpeed then
-        if targetNode.Name == "500" then
-            swtOver = false
-            entity:Destroy()
-            --error("This isn't an actual error, this is just to stop the script execution.")
-            canKill = false
-            return
-        end
-        if targetNode.Name == "1" then
-            if tonumber(targetNode.Parent.Name) == nodeRoom then
-                wait(reboundWaitTime)
-                folder = nodeRoom
-                rebounding = false
-                targetNode = workspace.entityNodes[tostring(folder)]["1"]
-            else
-                folder -= 1
-                local ln = -5
-                for i, v in workspace.entityNodes[tostring(folder)]:GetChildren() do
-                    if tonumber(v.Name) > ln then
-                        ln = tonumber(v.Name)
-                    end
-                end
-			    targetNode = workspace.entityNodes[tostring(folder)][tostring(ln)]
-            end
-        else
-            for i, v in targetNode.Parent:GetChildren() do
-		        if tonumber(v.Name) == tonumber(targetNode.Name) - 1 then
-		        	targetNode = v
-		        end
-		    end
-        end
-	end
---end)
-    end
-    end
---end
+end, function()
+	local entity = spawner.entityModel()
+	entity.Figure.Footsteps:Stop()
+	entity.Figure.FootstepsFar:Stop()
+	entity.SeekMusic.TimePosition = 92.5
 end)
+spawner.changeMainPartName("Figure")
+spawner.spawn()
